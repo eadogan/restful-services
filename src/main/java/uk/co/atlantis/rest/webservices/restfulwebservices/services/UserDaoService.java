90294b8022a56267777b31c9242925a86212a848
eadogan <e.ahmetdogan@gmail.com>
2022-01-13 12:48:26 +0000

    Create UserService

---
+++ src/main/java/uk/co/atlantis/rest/webservices/restfulwebservices/services/UserDaoService.java
@@ -0,0 +1,43 @@
+package uk.co.atlantis.rest.webservices.restfulwebservices.services;
+
+import org.springframework.stereotype.Component;
+import uk.co.atlantis.rest.webservices.restfulwebservices.model.User;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount = 3;
+
+    static {
+        users.add(new User(1, "Adam", new Date()));
+        users.add(new User(2, "Eve", new Date()));
+        users.add(new User(1, "Jack", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        if(user.getId()==null) {
+            user.setId(++usersCount);
+        }
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id) {
+        for(User user:users) {
+            if(user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
