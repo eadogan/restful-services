7e02e2053bf6431e14a6c25f0c9fc27504b479ad
eadogan <e.ahmetdogan@gmail.com>
2022-01-14 12:35:08 +0000

    Refactor and Add Delete Method

--- src/main/java/uk/co/atlantis/rest/webservices/restfulwebservices/services/UserDaoService.java
+++ src/main/java/uk/co/atlantis/rest/webservices/restfulwebservices/services/UserDaoService.java
@@ -5,6 +5,7 @@ import uk.co.atlantis.rest.webservices.restfulwebservices.model.User;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Component
@@ -32,9 +33,21 @@ public class UserDaoService {
         return user;
     }
 
-    public User findOne(int id) {
+    public User findById(int id) {
         for(User user:users) {
             if(user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User deleteById(int id) {
+        Iterator<User> iterator = users.iterator();
+        while (iterator.hasNext()) {
+            User user = iterator.next();
+            if(user.getId() == id) {
+                iterator.remove();
                 return user;
             }
         }
